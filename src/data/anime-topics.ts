c553a705e6bdc49f4209433e1c0db67d1e2c1747
yanfishrider <1747676030@qq.com>
2026-07-31 15:40:33 +0800

    fix: 圆点旋转改为 from/to 形式

--- src/data/anime-topics.ts
+++ src/data/anime-topics.ts
@@ -755,7 +755,7 @@ animate('circle.progress-1', {
 });
 // 圆点同步旋转
 animate('.dot-wrap', {
-  rotate: [0, 360],
+  rotate: { from: '-2turn', delay: 0 },
   duration: 2000,
   easing: 'easeInOutQuad',
   loop: true,
